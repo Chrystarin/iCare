4358d76093e186c950587f8869f79cd0004fb3ba
Chrystarin <chrystarin@gmail.com>
2019-09-24 12:45:33 +0800

    Header Update

--- js/script.js
+++ js/script.js
@@ -1,12 +1,31 @@
 $(document).ready(function(){
-    $(window).onscroll(Header());
-    function Header() {
-        $("header")
-          .on("click", function(){
-          $(this).addClass('animate');
-        })
-          .on("animationend", function(){
-          $(this).removeClass('animate');
-        });
-    });
+
+    $(window).on("scroll", function(){tr();})
+    
+    function tr(){
+      var scroll = $(window).scrollTop();
+      
+      if(scroll > 5){
+        console.log("greater");
+        $("header").addClass('animate');
+        $("header div").css("border-color","transparent");
+      }else{
+        $("header").removeClass('animate');
+        $("header > div").css("border-color","white");
+        console.log("lower");
+      }
+      // console.log(scroll);
+    }
+    
+
+    
+        
+    // $("header")
+    //   .on("click", function(){
+    //   $(this).addClass('animate');
+    // });
+    //   .on("animationend", function(){
+    //   $(this).removeClass('animate');
+    // });
+    
 });
