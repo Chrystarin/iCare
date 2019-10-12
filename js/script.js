b7c0d53ffcb4bae1539c4965cfd2f9764fe7c9e1
haroldcastillo <haroldcastillo272@gmail.com>
2019-10-12 09:46:49 +0800

    10/10/2019
    - TIMER animation end  do
    - COUNTER
    - background of the animal facts done
    - SECTION 1 Ways to contribute

--- js/script.js
+++ js/script.js
@@ -30,66 +30,106 @@ $(document).ready(function(){
         }, 'slow');
     });
     // Animation for the header(Header)
-    var i = 0; 
-    Transition();
-    function Transition(){
-      if(i == title.length){
-        i = 0;
-      }
-      $("#Title,#Author,#Date,#Content").addClass("Change"); 
-      setTimeout(function(){
-        // insert of new value
-        $("#Title").html(title[i]);
-        $("#Author").html(author[i]);
-        $("#Date").html(date[i]);
-        $("#Content").html(content[i]);
-        $("#head").css("background",BG[i]);
+    var i = 0;
+    
+
+    
+    // inserting of value
+    insertion(i);
+    // add of the animation in timer
+    start();
+    function start(){
+      $("#hold").addClass("ani");
+      $("#hold").on("animationend",function(){
         i++;
+        $("#hold").removeClass(); 
         setTimeout(function(){
-          $("#Title,#Author,#Date,#Content").removeClass();         
-          // Time of the animation
-        },1000)
-        // time of the animation * 0.4
-      },400);
-      setTimeout(function(){
-        Transition();
-      },10000);
-      // for the changing of the content
-      
-      
+          $("#hold").addClass("ani");
+        });
+        if(i == title.length){
+          i = 0;
+        }
+        $("#Title,#Author,#Date,#Content").addClass("Change"); 
+        setTimeout(function(){
+         // insert of new value
+          insertion(i);
+        },400);
+        $("#Title,#Author,#Date,#Content").on("animationend",function(){
+          $("#Title,#Author,#Date,#Content").removeClass();
+        });
+      });
     }
-    FactsChange();
+    function insertion(x){
+      $("#Title").html(title[x]);
+      $("#Author").html(author[x]);
+      $("#Date").html(date[x]);
+      $("#Content").html(content[x]);
+      $("#head").css("background",BG[x]);
+    } 
     var c = 0;
-    function FactsChange(){
-      
-      $("#FactsContent").addClass("FactAni");
-      setTimeout(function(){
-        if(c==facts.length){
+    FactsInsertion(c);
+    // add of the animation in timer
+    startFacts();
+    function startFacts(){
+      $("#S2 > div > div").addClass("Startani");
+      $("#S2 > div > div").on("animationend",function(){
+        c++;
+        $("#S2 > div > div").removeClass(); 
+        setTimeout(function(){
+          $("#S2 > div > div").addClass("Startani");
+        });
+        if(c == facts.length){
           c = 0;
         }
-        // insert of new value
-        $("#FactsContent").html(facts[c]);
-        // $("").html(img[c]);
-        c++;
+        $("#FactsContent").addClass("FactAni"); 
         setTimeout(function(){
-          $("#FactsContent").removeClass();         
-          // Time of the animation
-        },1000);
-        // time of the animation * 0.4
-      }, 400);
-      setTimeout(function(){
-        FactsChange();
-      },10000);
-      
+         // insert of new value
+          FactsInsertion(c);
+        },400);
+        $("#FactsContent").on("animationend",function(){
+          $("#FactsContent").removeClass();
+        });
+      });
     }
-    
+    function FactsInsertion(x){
+      $("#FactsContent").html(facts[x]);
+    } 
+
+    counter($("#try"));
+
+    // function counter(x){
+    //   Num = x.html();
+    //   if(Num>0){
+    //     var c = 0;
+    //     x.html(c);
+    //     S();
+    //     function S(){
+    //       if(c < Num){
+    //        x.addClass("Conterani");
+    //        setTimeout(function(){
+    //         x.html(c);
+    //         c++;
+    //        },400);
+    //        x.on("animationend",function(){
+    //         x.removeClass();
+    //        });
+    //       }else{
+
+    //       }
         
-    // $("header")
-    //   .on("click", function(){
-    //   $(this).addClass('animate');
-    // });
-    //   .on("animationend", function(){
-    //   $(this).removeClass('animate');
-    // });
-    
+    //     }
+
+
+    //     // for(var c = 0; c <= Num; c++ ){
+    //     //   x.addClass("Counterani");
+    //     //   setTimeout(function(){
+    //     //     alert(c);
+    //     //     x.html(c);
+    //     //   },400);
+    //     //   x.on("animationend",function(){
+    //     //     x.removeClass();
+    //     //   });
+    //     // }
+    //   }
+    // }
 });
